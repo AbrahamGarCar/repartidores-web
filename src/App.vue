29acc714bd1dc702aeed791b38fad63407c04344
Gerardo Lucero <gera_conecta@hotmail.com>
2021-01-28 10:26:01 -0700

    se empezo el home

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,44 @@
 <style>
-    .navbar{
-        
+    .nav-link{
+        font-weight: bold;
+    }
+
+    .my-order{
+        color: white;
+        background-color: #F29F05;
     }
 </style>
 
 <template>
     <section>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: white;">
             <div class="container">
-                <router-link class="navbar-brand" to="/">Navbar</router-link>
+                <router-link class="navbar-brand" to="/">
+                    <img width="150px" src="./assets/images/logo.png" alt="">
+                </router-link>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item">
+                            <router-link class="nav-link" to="/">Inicio</router-link>
+                        </li>
+                        <li class="nav-item">
                             <router-link class="nav-link" to="/restaurantes">Restaurantes</router-link>
                         </li>
                         <li class="nav-item">
-                            <router-link class="nav-link" to="/mi-pedido">Mi pedido</router-link>
-                        </li>
-                        <li class="nav-item">
                             <router-link class="nav-link" to="/nosotros">Nosotros</router-link>
+                        </li>
+                        <li class="nav-item my-order">
+                            <router-link class="nav-link" style="color: white;" to="/mi-pedido">Mi pedido</router-link>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
-        <main class="container">
-            <div class="row">
-                <router-view/>
-            </div>
+        <main>
+            <router-view/>
         </main>
     </section>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,86 @@
+<style scoped>
+    .image-one-container{
+        width: 100%;
+        height: 650px;
+        position: relative;
+    }
+
+    .image-one-container img{
+        width: 100%;
+        height: 650px;
+        top: 0;
+        left: 0;
+        position: absolute;
+        object-fit: cover;
+        object-position: center;
+        z-index: 50;
+    }
+
+    .image-one-container .layer{
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 40%;
+        height: 650px;
+        background-color: rgba(166, 3, 3, 0.9);
+        z-index: 100;
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+        flex-direction: column;
+        color: white;
+        padding: 10px;
+    }
+
+    .btn-order{
+        padding: 10px 20px;
+        color: white;
+        background-color: #F29F05;
+        border-radius: 0px;
+        border: none;
+    }
+
+    .delivery-man-image{
+        width: 100%;
+        height: 400px;
+    }
+
+    .delivery-man-image img{
+        width: 100%;
+        height: 400px;
+        position: absolute;
+        object-fit: cover;
+    }
+</style>
+
 <template>
-    <section class="col-md-12">
-        <h1>Home</h1>
+    <section>
+        <!-- banner section -->
+        <div class="container-fluid banner-1">
+            <div class="row">
+                <div class="col-md-12 image-one-container">
+                    <img src="https://images.unsplash.com/photo-1541544741938-0af808871cc0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1349&q=80" alt="">
+                    <div class="layer">
+                        <h2>FASSST</h2>
+                        <h4>SERVICE - SATISFY - SPECIALIZE</h4>
+
+                        <button class="btn-order">ORDENA AHORA</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container mt-4">
+            <div class="row">
+                <div class="col-md-6 d-flex justify-content-center align-items-center flex-column delivery-man-image">
+                    <h2>Siempre alguien dispuesto a ir por tu comida.</h2>
+                    <h5>¿Que esperas para disfrutar de algo delicioso?</h5>
+                </div>
+                <div class="col-md-6 delivery-man-image">
+                    <img src="https://images.unsplash.com/photo-1548695607-9c73430ba065?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1484&q=80" alt="">
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -9,7 +89,3 @@ export default {
 
 }
 </script>
-
-<style>
-
-</style>
